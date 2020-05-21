11e1c9d6d9c5337e069b97b3e7e3f15ea09b092a
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-05-21 19:39:31 -0300

    Adição da activity de resumo de pacotes

--- app/src/main/java/alura/com/br/ui/activity/PagamentoActivity.java
+++ app/src/main/java/alura/com/br/ui/activity/PagamentoActivity.java
@@ -2,15 +2,36 @@ package alura.com.br.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import alura.com.br.R;
+import alura.com.br.model.Pacote;
+import alura.com.br.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITLEAPPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+        setTitle(TITLEAPPBAR);
+
+        Pacote pacote = new Pacote("São Paulo", "sao_paulo_sp", 2,
+                new BigDecimal("243.99"));
+        mostraPreco(pacote);
+        Intent intent = new Intent(this, ResumoCompraActivity.class);
+        startActivity(intent);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco);
+        String moedaBR = MoedaUtil.formaMoeda(pacote.getPreco());
+        preco.setText(moedaBR);
     }
 }
